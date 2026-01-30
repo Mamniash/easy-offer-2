c2302740d25b4f2a243908c33df849936ac06ba4
Александр <127408492+Mamniash@users.noreply.github.com>
2026-01-30 14:08:44 +0300

    Update pro subscription checkout

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   env: {
     DEMO_URL: process.env.DEMO_URL,
+    LINK_TO_WEEK: process.env.LINK_TO_WEEK,
+    LINK_TO_MONTH: process.env.LINK_TO_MONTH,
+    LINK_TO_YEAR: process.env.LINK_TO_YEAR,
   },
 };
 
