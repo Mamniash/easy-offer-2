f2d0bff9859fe6a93776d59a5a52a115b757f5e3
Mamniash <mamniashvili2003@gmail.com>
2026-03-19 11:58:56 +0300

    test 1

--- lib/subscription.ts
+++ lib/subscription.ts
@@ -41,6 +41,7 @@ const PRO_IDENTIFIERS = new Set([
   "mmohnov",
   "www_3dprintprops_com",
   "ponytabl",
+  "Kkorotkovi",
 ]);
 
 const PRO_TELEGRAM_IDS = new Set([2101651535, 975378496, 337548443]);
